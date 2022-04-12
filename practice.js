09081ef8e12c98407901d1f51fcfe891c765d2ec
kimsongsub <busanbak2000@naver.com>
2022-04-12 20:27:34 +0900

    apply useState()
    입력한 데이터를 useState의 두번째 인자(setMinutes)를 통해 첫번째 인자(minutes)를 변화시켜 화면에 출력

--- practice.js
+++ practice.js
@@ -1,27 +1,35 @@
 // let counter = 0;
 const root = document.getElementById("root");
 
-const countUp = () => {
-  counter += 1;
-  render();
-};
+// const countUp = () => {
+//   counter += 1;
+//   render();
+// };
 
 const Container = () => {
-  const [value, handler] = React.useState("first");
+  const [value, setValue] = React.useState("first");
   const countUp2 = () => {
-    handler(value + "!");
+    setValue(value + "!");
   };
+  const [minutes, setMinutes] = React.useState("");
+  const detectChange = (event) => {
+    setMinutes(event.target.value);
+  };
+
   return (
     <div>
       <h3>Total clicks: {value}</h3>
-      <button
-        style={{
-          backgroundColor: "tomato",
-        }}
-        onClick={countUp2}
-      >
-        Click Me!
-      </button>
+      <button onClick={countUp2}>Click Me!</button>
+      <label htmlFor="minutes">Minutes</label>
+      <input
+        id="minutes"
+        placeholder="Minutes"
+        type="number"
+        onChange={detectChange}
+      ></input>
+      <label htmlFor="hours">hours</label>
+      <input id="hours" placeholder="Hours" type="number"></input>
+      <h3>You input {minutes}</h3>
     </div>
   );
 };
